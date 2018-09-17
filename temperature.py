6e649db9c03285500f2ea845a41eed0b7c2895f1
alexanderqchen <aqchen@ucla.edu>
2018-09-17 18:13:05 +0000

    Added automated text messaging when temperature is out of range

--- temperature.py
+++ temperature.py
@@ -6,10 +6,17 @@ import time
 from datetime import datetime
 import mraa
 import math
+from twilio.rest import Client
+from twilio_creds import ACCOUNT_SID, AUTH_TOKEN, TO_NUMBER, FROM_NUMBER
+
+MIN_TEMP = 40
+MAX_TEMP = 70
 
 B = 4275
 R0 = 100000
 TEMP_MODULE = mraa.Aio(1)
+
+TWILIO_CLIENT = Client(ACCOUNT_SID, AUTH_TOKEN)
 
 SCOPES = 'https://www.googleapis.com/auth/spreadsheets'
 SPREADSHEET_ID = '190SkqjfInZmjhJmKWogohXDzLyzp40EhpMqDPC9zWdo'
@@ -17,6 +24,8 @@ SPREADSHEET_ID = '190SkqjfInZmjhJmKWogohXDzLyzp40EhpMqDPC9zWdo'
 def main():
 	while(True):
 		temperature = getTemp()
+		if temperature < MIN_TEMP or temperature > MAX_TEMP:
+			sendText(temperature)
 		appendToSheet(temperature)
 		time.sleep(2)
 
@@ -29,6 +38,12 @@ def getTemp():
 	fahrenheit = (9.0/5) * celsius + 32
 
 	return fahrenheit
+
+def sendText(temperature):
+	message = TWILIO_CLIENT.messages.create(
+		to = TO_NUMBER,
+		from_ = FROM_NUMBER,
+		body = "Refrigerator temperature is out of optimal range at " + str(temperature) + " degrees Fahrenheit.")
 
 def appendToSheet(temperature):
 	print('appending to google sheet')
